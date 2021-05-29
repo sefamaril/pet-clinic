6184e70d8cb6b6d52009c547404646e2318287b3
sefamaril <sefamaril@eskisehir.edu.tr>
2021-05-29 22:23:58 +0300

    Initial Project Add

---
+++ src/test/java/maril/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package maril.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
